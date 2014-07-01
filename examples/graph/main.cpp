0d66fbb41789aa274b5acb726ad6c8948ab42d66
Tony Grochow <hewa4632@hektor.(none)>
2014-07-01 21:09:27 +0200

    advanced example

--- examples/graph/main.cpp
+++ examples/graph/main.cpp
@@ -1,76 +1,108 @@
-/******************************************************************************
-* guacamole - delicious VR *
-* *
-* Copyright: (c) 2011-2013 Bauhaus-Universität Weimar *
-* Contact: felix.lauer@uni-weimar.de / simon.schneegans@uni-weimar.de *
-* *
-* This program is free software: you can redistribute it and/or modify it *
-* under the terms of the GNU General Public License as published by the Free *
-* Software Foundation, either version 3 of the License, or (at your option) *
-* any later version. *
-* *
-* This program is distributed in the hope that it will be useful, but *
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License *
-* for more details. *
-* *
-* You should have received a copy of the GNU General Public License along *
-* with this program. If not, see <http://www.gnu.org/licenses/>. *
-* *
-******************************************************************************/
 
 #include <gua/guacamole.hpp>
 #include <gua/renderer/GraphLoader.hpp>
 
-#include <thread>
-#include <chrono>
+std::shared_ptr<gua::Node> 
 
-int main(int argc, char** argv)
+init_graph(gua::SceneGraph * scene_graph)
 {
-  gua::init(argc, argv);
-
-  gua::ShadingModelDatabase::load_shading_models_from("data/materials/");
-  gua::MaterialDatabase::load_materials_from("data/materials/");
-
-  gua::SceneGraph scene_graph("main_scene");
-
   gua::GraphLoader loader;
-  auto graph_geometry(loader.create("graph_visoalization","data/materials/Red.gmd"));
 
-  auto graph = scene_graph.add_node("/",graph_geometry);
+  std::shared_ptr<gua::Node>
 
-  auto light = scene_graph.add_node<gua::PointLightNode>("/","light");
-  light->scale(5.f);
-  light->translate(0, 1.f, 1.f);
+  graph_node(loader.create("graph","data/materials/Red.gmd"));
 
-  auto screen = scene_graph.add_node<gua::ScreenNode>("/","screen");
+  return scene_graph->add_node(scene_graph->get_root(),graph_node);
+}
+
+std::shared_ptr<gua::PointLightNode> 
+
+init_light(gua::SceneGraph * scene_graph)
+{
+  return scene_graph->
+
+  add_node<gua::PointLightNode>(scene_graph->get_root(),"light");
+}
+
+std::shared_ptr<gua::ScreenNode> 
+
+init_screen(gua::SceneGraph * scene_graph)
+{
+  std::shared_ptr<gua::ScreenNode> screen = 
+
+  scene_graph->add_node<gua::ScreenNode>(scene_graph->get_root(),"screen");
+
   screen->data.set_size(gua::math::vec2(1.6f,0.9f));
-	screen->translate(0.0f, 0.0f, 5.0f);
+  screen->translate(0.0f,0.0f,1.0f);
 
-  auto eye = scene_graph.add_node<gua::TransformNode>("/screen", "eye");
-  eye->translate(0, 0, 7.0);
+  return screen;
+}
 
-  auto pipe = new gua::Pipeline();
-  pipe->config.set_camera(gua::Camera("/screen/eye","/screen/eye",
-																			"/screen","/screen",
-																			"main_scene"));
+std::shared_ptr<gua::TransformNode>
+
+init_eye(gua::SceneGraph * scene_graph)
+{
+  std::shared_ptr<gua::TransformNode> eye =
+
+  scene_graph->add_node<gua::TransformNode>(scene_graph->get_root(),"eye");
+  eye->translate(0.0f,0.0f,2.5f);
+
+  return eye;
+}
+
+void pipe_config(gua::Pipeline * pipe)
+{
+  unsigned const width = 1600 , height = 900;
+
+  gua::Camera camera("eye","eye","screen","screen","scene");
+  pipe->config.set_camera(camera);
+
+  pipe->config.set_left_resolution(gua::math::vec2ui(width,height));
   pipe->config.set_enable_fps_display(true);
+  pipe->config.set_enable_preview_display(true);
 
-  pipe->config.set_enable_backface_culling(false);
-	pipe->config.set_enable_preview_display(true);
+  gua::Window * window = new gua::Window();
+  window->config.set_left_resolution(gua::math::vec2ui(width,height));
+  window->config.set_size(gua::math::vec2ui(width,height));
+  pipe->set_window(window);
+}
 
-  pipe->set_window(new gua::Window());
-	pipe->get_window()->config.set_left_resolution(gua::math::vec2ui(1600, 1200));
-  pipe->get_window()->config.set_size(gua::math::vec2ui(1600, 1200));
-  pipe->config.set_left_resolution(gua::math::vec2ui(1600, 1200));
+int main(int argc,char ** argv)
+{
+  gua::init(argc,argv);
 
-  gua::Renderer renderer({pipe});
+  gua::SceneGraph * scene_graph = new gua::SceneGraph("scene");
 
-  while (true)
-	{
-    std::this_thread::sleep_for(std::chrono::milliseconds(100/6));
-    renderer.queue_draw({&scene_graph});
-  }
+  auto graph_node(init_graph(scene_graph));
+  auto light_node(init_light(scene_graph));
+  auto screen_node(init_screen(scene_graph));
+  auto eye_node(init_eye(scene_graph));
+
+  gua::Pipeline * pipe(new gua::Pipeline);
+  pipe_config(pipe);
+  std::vector<gua::Pipeline *> pipes;
+  pipes.insert(pipes.begin(),pipe);
+  gua::Renderer renderer(pipes);
+
+
+  double const frame_time = 1.0f / 60.0f;
+
+  gua::events::MainLoop main_loop;
+  gua::events::Ticker ticker(main_loop,frame_time);
+
+  std::vector<gua::SceneGraph const*> entire_scene;
+  entire_scene.insert(entire_scene.begin(),scene_graph);
+
+  std::function<void(void)> draw = 
+
+  [&]()
+  {
+    renderer.queue_draw(entire_scene);
+  };
+
+  ticker.on_tick.connect(draw);
+
+  main_loop.start(); 
 
   return 0;
 }
